9ab62fe1bd5a7ed5d11723d14b57a3f6a2ed6000
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2019-04-04 13:41:38 +0100

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -17,3 +17,18 @@ start_time = time.time()
 print(alist)
 end_time = time.time()
 time_elaspsed = end_time - start_time
+
+=================================================================================================================================
+def insertion_sort(A):
+    for i in range(1, len(A)):
+       curNum = A[i]
+       for j in range(i - 1, 0, -1):
+           if A[j] > curNum:
+               A[j+1] = A[j]
+    else:
+                A[j+1] = curNum
+                
+
+A = [54,26,93,17,77,31,44,55,20]
+
+print(A)
