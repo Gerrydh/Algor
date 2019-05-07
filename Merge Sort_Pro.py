2a996f49e4984ee42aabead1754c48b34b93d646
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2019-05-07 09:19:36 +0100

    Create Merge Sort_Pro.py

---
+++ Merge Sort_Pro.py
@@ -0,0 +1,61 @@
+# Ger Hanlon- Merge Sort. April 2019
+# Source Code- https://www.pythoncentral.io/merge-sort-implementation-guide/
+
+
+import time
+
+start_time = time.time()
+
+def mergeSort(alist):
+
+   print("Splitting ",alist)
+# split the unsorted list into two groups, until there is 1 element per group
+
+   if len(alist)>1:
+       mid = len(alist)//2
+       lefthalf = alist[:mid]
+       righthalf = alist[mid:]
+
+       
+       mergeSort(lefthalf)
+       mergeSort(righthalf)
+
+# compare each of the elements and then group them together
+# repeat this process until the whole list is merged and sorted
+
+       i=0
+       j=0
+       k=0
+
+
+       while i < len(lefthalf) and j < len(righthalf):
+           if lefthalf[i] < righthalf[j]:
+               alist[k]=lefthalf[i]
+               i=i+1
+           else:
+               alist[k]=righthalf[j]
+               j=j+1
+           k=k+1
+
+       while i < len(lefthalf):
+           alist[k]=lefthalf[i]
+           i=i+1
+           k=k+1
+
+       while j < len(righthalf):
+           alist[k]=righthalf[j]
+           j=j+1
+           k=k+1
+
+   print("Merging ",alist)
+num_runs = 20
+results = []
+for r in range (num_runs):
+
+    alist = []
+mergeSort(alist)
+print(alist)
+
+end_time = time.time()
+time_elapsed = end_time - start_time
+print(time_elapsed)
